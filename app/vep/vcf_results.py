89bf32fbe070dad5e3ddc900dc9f5b179a87f734
Jon Keatley <jon@ebi.ac.uk>
2024-06-09 23:19:50 +0100

    Added some more details to alt allele. Need a function for getting csq values as there is a clear pattern

--- app/vep/vcf_results.py
+++ app/vep/vcf_results.py
@@ -1,6 +1,8 @@
+from typing import Dict
+
 import vcfpy
 
-from ..vep.model import Variant, Location, ReferenceVariantAllele, Metadata, PaginationMetadata, VepResultsResponse
+from ..vep.model import *
 
 TARGET_COLUMNS = [
                     "Allele", "AF", "Consequence", "Feature", "Feature_type",
@@ -21,6 +23,53 @@ def get_prediction_index_map(csq_header:str, target_columns=TARGET_COLUMNS) -> d
     
     return map
 
+def get_alt_allele_details(alt:vcfpy.AltRecord,rec:vcfpy.Record,index_map:Dict) -> AlternativeVariantAllele:
+    
+    frequency = None
+    consequences = []
+    
+    for str_csq in rec.INFO["CSQ"]:
+            csq_values = str_csq.split("|")
+            
+            if csq_values[index_map["Allele"]] != alt.value: 
+                continue
+                
+            if "AF" in index_map and csq_values[index_map["AF"]]:
+                frequency = csq_values[index_map["AF"]]
+                
+            if csq_values[index_map["Feature_type"]] == "Transcript": 
+                
+                is_cononical = True if "CANONICAL" in index_map and csq_values[index_map["CANONICAL"]] == "YES" else False
+                
+                cons = []
+                if "Consequence" in index_map and csq_values[index_map["Consequence"]]: 
+                    cons = csq_values[index_map["Consequence"]].split("&")
+                 
+                #Do we all ways have a strand? check!   
+                strand = Strand.reverse if "STRAND" in index_map and csq_values[index_map["STRAND"]] == "-1" else Strand.forward
+                
+                biotype = csq_values[index_map["BIOTYPE"]] if "BIOTYPE" in index_map and csq_values[index_map["BIOTYPE"]] else ""
+                
+                consequences.append(
+                    PredictedTranscriptConsequence(
+                    feature_type= FeatureType.transcript,
+                    stable_id = "",
+                    gene_stable_id = "",
+                    biotype = biotype,
+                    is_canonical = is_cononical,
+                    consequences= cons,
+                    strand= strand,
+                    )
+                )
+                      
+    return AlternativeVariantAllele(
+        allele_sequence=alt.value,
+        allele_type=alt.type,
+        representative_population_allele_frequency=frequency,
+        predicted_molecular_consequences= consequences
+    )
+            
+
 def get_results(page_size:int,page:int,vcf_path:str) -> VepResultsResponse: 
     # Check file file exists 
     
@@ -38,8 +87,8 @@ def get_results(page_size:int,page:int,vcf_path:str) -> VepResultsResponse:
     variants = []
     count = 0
     
+    #populate page
     for record in vcf_records: 
-        
         chrom = record.CHROM
         if chrom.startswith("chr"):
             chrom = chrom[3:]
@@ -54,12 +103,16 @@ def get_results(page_size:int,page:int,vcf_path:str) -> VepResultsResponse:
         rva = ReferenceVariantAllele(
             allele_sequence = record.REF
         )
+        
+        alt_alleles = [
+            get_alt_allele_details(alt,record, prediction_index_map) for alt in record.ALT
+        ]
             
         v = Variant(
             name = ";".join(record.ID) if len(record.ID) > 0 else ".",
             location = l,
             reference_allele = rva,
-            alternative_alleles= []
+            alternative_alleles= alt_alleles
         )
         variants.append(v)
         

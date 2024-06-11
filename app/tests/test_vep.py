e0278ad6c2ff38e0d83aa84e2a02b3c4ed78d8c4
Jon Keatley <jon@ebi.ac.uk>
2024-06-11 14:00:28 +0100

    Added streaming option for get_results. Started building actual tests. Ran code through black

--- app/tests/test_vep.py
+++ app/tests/test_vep.py
@@ -1,10 +1,22 @@
 import json
+from io import StringIO
 
 import pytest
 
 from ..main import app, get_db_path
 
-from ..vep.vcf_results import get_results, _get_prediction_index_map, TARGET_COLUMNS
+from ..vep.vcf_results import get_results_from_path, get_results_from_stream, _get_prediction_index_map, TARGET_COLUMNS
+
+CSQ_DESCRIPTION = "Consequence annotations from Ensembl VEP. Format: Allele|Consequence|IMPACT|SYMBOL|Gene|Feature_type|Feature|BIOTYPE|EXON|INTRON|HGVSc|HGVSp|cDNA_position|CDS_position|Protein_position|Amino_acids|Codons|Existing_variation|REF_ALLELE|UPLOADED_ALLELE|DISTANCE|STRAND|FLAGS|SYMBOL_SOURCE|HGNC_ID|CANONICAL|SIFT|PolyPhen|AF|CLIN_SIG|SOMATIC|PHENO|MOTIF_NAME|MOTIF_POS|HIGH_INF_POS|MOTIF_SCORE_CHANGE|TRANSCRIPTION_FACTORS"
+
+TEST_VCF = f"""##fileformat=VCFv4.2
+##fileDate=20160824
+##INFO=<ID=CSQ,Number=.,Type=String,Description="{CSQ_DESCRIPTION}">
+#CHROM	POS	ID	REF	ALT	QUAL	FILTER	INFO
+chr19	82664	.	C	T	50	PASS	CSQ=T|upstream_gene_variant|MODIFIER|FAM138F|ENSG00000282591|Transcript|ENST00000631376.1|lncRNA||||||||||rs868831437|C|C/T|4978|-1||HGNC|HGNC:33581|YES|||0.4860||||||||
+chr19	82829	my_var	T	A	50	PASS	CSQ=A|intergenic_variant|MODIFIER|||||||||||||||rs1555675005|T|T/A|||||||||||||||||
+
+"""
 
 
 def test_load_csq_description_happy():
@@ -15,12 +27,35 @@ def test_load_csq_description_happy():
     
     prediction_index_map = _get_prediction_index_map(csq_header)
     assert  prediction_index_map == expected_index
+    
+def test_get_results_from_stream(): 
+    
+    results = get_results_from_stream(100,0,StringIO(TEST_VCF))
+    
+    expected_index = {TARGET_COLUMNS[x]:x for x in range(0,len(TARGET_COLUMNS))}
+    
+    assert len(results.variants) == 2
+    
+    assert results.variants[0].name == "."
+    assert results.variants[1].name == "my_var"
+    
+    assert results.variants[0].reference_allele.allele_sequence == "C"
+    assert results.variants[1].reference_allele.allele_sequence == "T"
+    
+    assert results.variants[0].alternative_alleles[0].allele_sequence == "T"
+    assert results.variants[0].alternative_alleles[0].allele_type == "SNV"
+    
+    
+    assert results.variants[0].alternative_alleles[0].representative_population_allele_frequency == 0.4860
+    assert results.variants[1].alternative_alleles[0].representative_population_allele_frequency == None
+    
+
 
 @pytest.mark.skip(reason="Used to test against a real VCF file")
-def test_get_results_with_file(): 
+def test_get_results_with_file_and_dump(): 
 
     vcf_path = "/Users/jon/Programming/vep-vcf-results/vep-output-phase1-options-plus-con.vcf"
-    results = get_results(100,2,vcf_path)
+    results = get_results_from_path(100,2,vcf_path)
     
     expected_index = {TARGET_COLUMNS[x]:x for x in range(0,len(TARGET_COLUMNS))}
     
--- app/vep/vcf_results.py
+++ app/vep/vcf_results.py
@@ -1,192 +1,216 @@
-from typing import Dict, Any
+from typing import Dict, Any, IO, List
 
 import vcfpy
 
 from ..vep.model import *
 
 TARGET_COLUMNS = [
-                    "Allele", "AF", "Consequence", "Feature", "Feature_type",
-                    "BIOTYPE", "CANONICAL", "SYMBOL","Gene", "STRAND",
-                    "IMPACT"
-                ]
-                
-#Taken from https://github.com/Ensembl/ensembl-hypsipyle/blob/main/common/file_model/variant.py#L142
-#Needs to be moved into a shared module
-def _set_allele_type(alt_one_bp: bool, ref_one_bp: bool, ref_alt_equal_bp: bool):         
-        match [alt_one_bp, ref_one_bp, ref_alt_equal_bp]:
-            case [True, True, True]: 
-                allele_type = "SNV" 
-                SO_term = "SO:0001483"
+    "Allele",
+    "AF",
+    "Consequence",
+    "Feature",
+    "Feature_type",
+    "BIOTYPE",
+    "CANONICAL",
+    "SYMBOL",
+    "Gene",
+    "STRAND",
+    "IMPACT",
+]
 
-            case [True, False, False]: 
-                allele_type = "deletion" 
-                SO_term = "SO:0000159"
 
-            case [False, True, False]: 
-                allele_type = "insertion"
-                SO_term = "SO:0000667"
+# Taken from https://github.com/Ensembl/ensembl-hypsipyle/blob/main/common/file_model/variant.py#L142
+# Needs to be moved into a shared module
+def _set_allele_type(alt_one_bp: bool, ref_one_bp: bool, ref_alt_equal_bp: bool):
+    """Create a allele type for a variant based on Variation teams logic using ref and largest alt allele sizes"""
+    match [alt_one_bp, ref_one_bp, ref_alt_equal_bp]:
+        case [True, True, True]:
+            allele_type = "SNV"
+            SO_term = "SO:0001483"
 
-            case [False, False, False]: 
-                allele_type = "indel"
-                SO_term = "SO:1000032"
+        case [True, False, False]:
+            allele_type = "deletion"
+            SO_term = "SO:0000159"
 
-            case [False, False, True]: 
-                allele_type = "substitution"
-                SO_term = "SO:1000002"   
-        return allele_type, SO_term
-        
-def _get_prediction_index_map(csq_header:str, target_columns=TARGET_COLUMNS) -> dict: 
-    
+        case [False, True, False]:
+            allele_type = "insertion"
+            SO_term = "SO:0000667"
+
+        case [False, False, False]:
+            allele_type = "indel"
+            SO_term = "SO:1000032"
+
+        case [False, False, True]:
+            allele_type = "substitution"
+            SO_term = "SO:1000002"
+    return allele_type, SO_term
+
+
+def _get_prediction_index_map(csq_header: str, target_columns=TARGET_COLUMNS) -> dict:
+    """Creates a dictionary of column indexes based on the CSQ info description"""
     csq_header = csq_header.split(":")[-1].strip()
     csq_headers = csq_header.split("|")
-           
+
     map = {
-        csq_headers[index]:index for index in range(len(csq_headers)) if csq_headers[index] in target_columns
+        csq_headers[index]: index
+        for index in range(len(csq_headers))
+        if csq_headers[index] in target_columns
     }
-    
-    ##TODO add exception if len(map) != len(TARGET_COLUMNS)
-    
+
+    # TODO add exception if len(map) != len(TARGET_COLUMNS)
+
     return map
 
-def _get_csq_value(csq_values:List, csq_key:str, default_value:Any, index_map:Dict) -> Any:
-    if csq_key in index_map and csq_values[index_map[csq_key]]: 
+
+def _get_csq_value(
+    csq_values: List, csq_key: str, default_value: Any, index_map: Dict
+) -> Any:
+    """Helper method to return CSQ values or a default value if either the key or the value is missing"""
+    if csq_key in index_map and csq_values[index_map[csq_key]]:
         return csq_values[index_map[csq_key]]
     return default_value
-    
 
-def _get_alt_allele_details(alt:vcfpy.AltRecord,rec:vcfpy.Record,index_map:Dict) -> AlternativeVariantAllele:
-    
+
+def _get_alt_allele_details(
+    alt: vcfpy.AltRecord, rec: vcfpy.Record, index_map: Dict
+) -> AlternativeVariantAllele:
+    """Creates  AlternativeVariantAllele based on target alt allele and CSQ entires"""
     frequency = None
     consequences = []
-    
+
     for str_csq in rec.INFO["CSQ"]:
-            csq_values = str_csq.split("|")
-            
-            if csq_values[index_map["Allele"]] != alt.value: 
-                continue
-                
-            frequency = _get_csq_value(csq_values, "AF", None, index_map)
-            
-            cons = _get_csq_value(csq_values, "Consequence", "", index_map).split("&") 
-            if csq_values[index_map["Feature_type"]] == "Transcript": 
-                
-                is_cononical = _get_csq_value(csq_values, "CANONICAL", "NO", index_map) == "YES" 
+        csq_values = str_csq.split("|")
 
-                #It looks like for Feature_type = Transcript that we always have a STRAND value  
-                strand = Strand.reverse if _get_csq_value(csq_values, "STRAND", "1", index_map) == "-1" else Strand.forward
+        if csq_values[index_map["Allele"]] != alt.value:
+            continue
 
-                consequences.append(
-                    PredictedTranscriptConsequence(
-                    feature_type= FeatureType.transcript,
-                    stable_id = _get_csq_value(csq_values, "Feature", "", index_map),
-                    gene_stable_id = _get_csq_value(csq_values, "Gene", "", index_map),
-                    biotype = _get_csq_value(csq_values, "BIOTYPE", "", index_map),
-                    is_canonical = is_cononical,
-                    gene_symbol = _get_csq_value(csq_values, "SYMBOL", "", index_map),
-                    consequences= cons,
-                    strand= strand,
-                    )
+        frequency = _get_csq_value(csq_values, "AF", None, index_map)
+
+        cons = _get_csq_value(csq_values, "Consequence", "", index_map).split("&")
+        if csq_values[index_map["Feature_type"]] == "Transcript":
+            is_cononical = (
+                _get_csq_value(csq_values, "CANONICAL", "NO", index_map) == "YES"
+            )
+
+            # It looks like for Feature_type = Transcript that we always have a STRAND value
+            strand = (
+                Strand.reverse
+                if _get_csq_value(csq_values, "STRAND", "1", index_map) == "-1"
+                else Strand.forward
+            )
+
+            consequences.append(
+                PredictedTranscriptConsequence(
+                    feature_type=FeatureType.transcript,
+                    stable_id=_get_csq_value(csq_values, "Feature", "", index_map),
+                    gene_stable_id=_get_csq_value(csq_values, "Gene", "", index_map),
+                    biotype=_get_csq_value(csq_values, "BIOTYPE", "", index_map),
+                    is_canonical=is_cononical,
+                    gene_symbol=_get_csq_value(csq_values, "SYMBOL", "", index_map),
+                    consequences=cons,
+                    strand=strand,
                 )
-            elif "intergenic_variant" in cons:
-                consequences.append(
-                    PredictedIntergenicConsequence(
-                        feature_type="",
-                        consequences = ['intergenic_variant'],
-                    )
-                )   
-                      
+            )
+        elif "intergenic_variant" in cons:
+            consequences.append(
+                PredictedIntergenicConsequence(
+                    feature_type="",
+                    consequences=["intergenic_variant"],
+                )
+            )
+
     return AlternativeVariantAllele(
         allele_sequence=alt.value,
         allele_type=alt.type,
         representative_population_allele_frequency=frequency,
-        predicted_molecular_consequences= consequences
+        predicted_molecular_consequences=consequences,
     )
-            
 
-def get_results(page_size:int,page:int,vcf_path:str) -> VepResultsResponse: 
-    # Check file file exists 
-    
-    # Load vcf 
-    vcf_records = vcfpy.Reader.from_path(vcf_path)
-    
-    # Parse csq header 
+
+def get_results_from_path(
+    page_size: int, page: int, vcf_path: str
+) -> VepResultsResponse:
+    """Helper method that converts a file path to a stream for use with get_results_from_stream"""
+    # Check file file exists
+    with open(vcf_path) as vcf_stream:
+        return get_results_from_stream(page_size, page, vcf_stream)
+
+
+def get_results_from_stream(
+    page_size: int, page: int, vcf_stream: IO
+) -> VepResultsResponse:
+    """Generates a page of VCF data in the format described in APISpecification.yaml for a given VCF stream"""
+
+    # Load vcf
+    vcf_records = vcfpy.Reader.from_stream(vcf_stream)
+
+    # Parse csq header
     csq_details = vcf_records.header.get_info_field_info("CSQ").description
     prediction_index_map = _get_prediction_index_map(csq_details)
-    
-    # handle offset 
+
+    # handle offset
     count = 0
     offset = page_size * page
-    
-    #This is very slow. We need to find a better way of handling this. vcfpy __next__ might be the key as it reads lines
-    for r in vcf_records:
-        count += 1
-        if count >= offset:
-            break
-        
-    #build page
+
+    # This is very slow. We need to find a better way of handling this. vcfpy __next__ might be the key as it reads lines
+    if offset > 0:
+        for r in vcf_records:
+            count += 1
+            if count >= offset:
+                break
+
+    # build page
     variants = []
     count = 0
-    
-    #populate page
-    for record in vcf_records: 
+
+    # populate page
+    for record in vcf_records:
         chrom = record.CHROM
         if chrom.startswith("chr"):
             chrom = chrom[3:]
-            
+
         ref_len = len(record.REF)
 
-        #https://github.com/bihealth/vcfpy/blob/697768d032b6b476766fb4c524c91c8d24559330/vcfpy/record.py#L63
-        #end does not look like it is implemented.
-        #Using https://github.com/Penghui-Wang/PyVCF/blob/master/vcf/model.py#L190 from competting vcf module 
+        # https://github.com/bihealth/vcfpy/blob/697768d032b6b476766fb4c524c91c8d24559330/vcfpy/record.py#L63
+        # end does not look like it is implemented.
+        # Using https://github.com/Penghui-Wang/PyVCF/blob/master/vcf/model.py#L190 from competting vcf module
         l = Location(
-            region_name = chrom,
-            start = record.begin,
-            end = record.end if record.end else record.begin + ref_len
+            region_name=chrom,
+            start=record.begin,
+            end=record.end if record.end else record.begin + ref_len,
         )
-            
-        rva = ReferenceVariantAllele(
-            allele_sequence = record.REF
-        )
-        
+
+        rva = ReferenceVariantAllele(allele_sequence=record.REF)
+
         longest_alt = len(max([a.value for a in record.ALT], key=len))
-        
-        
+
         alt_alleles = [
-            _get_alt_allele_details(alt,record, prediction_index_map) for alt in record.ALT
+            _get_alt_allele_details(alt, record, prediction_index_map)
+            for alt in record.ALT
         ]
-            
+
         v = Variant(
-            name = ";".join(record.ID) if len(record.ID) > 0 else ".",
-            location = l,
-            reference_allele = rva,
-            alternative_alleles= alt_alleles,
-            allele_type = _set_allele_type(longest_alt <2, ref_len < 2, longest_alt == ref_len)[0],
+            name=";".join(record.ID) if len(record.ID) > 0 else ".",
+            location=l,
+            reference_allele=rva,
+            alternative_alleles=alt_alleles,
+            allele_type=_set_allele_type(
+                longest_alt < 2, ref_len < 2, longest_alt == ref_len
+            )[0],
         )
         variants.append(v)
-        
-        count+= 1   
-        if count >= page_size: 
+
+        count += 1
+        if count >= page_size:
             break
-            
-    #Also very slow. We could compute this and add it to the VCF header
+
+    # Also very slow. We could compute this and add it to the VCF header
     total = offset + count
     for r in vcf_records:
         total += 1
-    
+
     meta = Metadata(
-        pagination = PaginationMetadata(
-            page=page,
-            per_page=page_size,
-            total= total
-        )
+        pagination=PaginationMetadata(page=page, per_page=page_size, total=total)
     )
-       
-         
-    return VepResultsResponse(
-        metadata= meta,
-        variants= variants
-    )
-    
-    
-    
-    
+
+    return VepResultsResponse(metadata=meta, variants=variants)

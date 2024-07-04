c1b9ecde73a60093b3668d57d175bf3e74847763
jyothishnt <joyjoy.nt@gmail.com>
2024-07-04 14:25:58 +0100

    Create vep config ini file

--- app/vep/models/pipeline_model.py
+++ app/vep/models/pipeline_model.py
@@ -1,8 +1,10 @@
 from typing import Optional, List, Literal
 from pydantic import BaseModel, validator
+
 class VEPConfigParams(BaseModel):
     vcf: str
     vep_config: str
+
 class LaunchParams(BaseModel):
     computeEnvId: str = "17wuWKrWOZW5JrLoGGwPdD"
     pipeline: str
@@ -17,5 +19,19 @@ class LaunchParams(BaseModel):
     labelIds: List[str]
     headJobCpus: int = 1
     headJobMemoryMb: int = 4
+
 class PipelineParams(BaseModel):
     launch: LaunchParams
+
+class ConfigIniParams(BaseModel):
+  cache: int = 1
+  dir_cache: str = "/nfs/production/flicek/ensembl/variation/data/VEP/tabixconverted/"
+  species: str = "homo_sapiens"
+  assembly: str = 'GRCh38'
+  cache_version: int = 110
+  offline: int = 1
+  force_overwrite: int = 1
+  symbol: int = 1
+  biotype: int = 1
+  transcript_version: int = 1
+  canonical: int = 1

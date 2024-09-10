d12337904fa54d1bfa2a0e0c0d1119dc084c2157
Kamal Dodiya <40833799+kamaldodiya@users.noreply.github.com>
2024-09-10 19:32:45 +0100

    Trust environment (#39)

--- app/core/config.py
+++ app/core/config.py
@@ -30,6 +30,7 @@ API_PREFIX = "/api/tools"
 
 config = Config(".env")
 DEBUG: bool = config("DEBUG", cast=bool, default=False)
+TRUST_ENV: bool = config("TRUST_ENV", cast=bool, default=True)
 PROJECT_NAME: str = config("PROJECT_NAME", default="Ensembl Web Tools API")
 ALLOWED_HOSTS: list[str] = config(
     "ALLOWED_HOSTS",

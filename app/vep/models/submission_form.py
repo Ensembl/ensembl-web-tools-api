b82afab640be09b687f4d40d77ae7ac8cc713ba3
Andres Veidenberg <andres@ebi.ac.uk>
2024-10-07 11:38:30 +0300

    Revert changes in submission_form

--- app/vep/models/submission_form.py
+++ app/vep/models/submission_form.py
@@ -1,10 +1,8 @@
-from pydantic import BaseModel
-
+from pydantic import BaseModel, Field
 
 class DropdownOption(BaseModel):
-    label: str
-    value: str
-
+  label: str
+  value: str
 
 class Dropdown(BaseModel):
     label: str
@@ -13,21 +11,17 @@ class Dropdown(BaseModel):
     options: list[DropdownOption]
     default_value: str
 
-
 class Checkbox(BaseModel):
     label: str
     description: str | None = None
     type: str = "boolean"
     default_value: bool = True
 
-
 class FormConfig(BaseModel):
     transcript_set: Dropdown
     symbol: Checkbox = Checkbox(label="Gene symbol")
     biotype: Checkbox = Checkbox(label="Transcript biotype")
 
-
 class GenomeAnnotationProvider(BaseModel):
-    annotation_provider_name: str
-    annotation_version: str
-    last_geneset_update: str
+  annotation_provider_name: str
+  annotation_version: str

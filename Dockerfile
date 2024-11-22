eae34bd1f69c2749df17b659ffa7760a703537b0
Andres Veidenberg <andres@ebi.ac.uk>
2024-11-22 04:55:35 +0000

    Integrate bcftools

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,14 @@
+FROM python:3.11 AS build
+RUN wget https://github.com/samtools/bcftools/releases/download/1.21/bcftools-1.21.tar.bz2; \
+  tar -x -f bcftools-1.21.tar.bz2; cd bcftools-1.21; make
+
 FROM python:3.11
 WORKDIR /app
 COPY ./requirements.txt requirements.txt
 RUN pip install --no-cache-dir --upgrade -r requirements.txt
 COPY ./app /app
 COPY ./data /data
+COPY --from=build /bcftools-1.21/bcftools /usr/local/bin/
 RUN mkdir -p /nfs/public/rw/enswbsites
 ENV TMPDIR /nfs/public/rw/enswbsites
 ENV PORT 8013
--- app/vep/utils/vcf_results.py
+++ app/vep/utils/vcf_results.py
@@ -1,9 +1,10 @@
 """ Module for loading a VCF and parsing it into a VepResultsResponse
 object as defined in APISpecification"""
 
+from io import StringIO
+import subprocess
+from pydantic import FilePath
 import vcfpy
-from typing import List, Dict, Any, IO
-
 from vep.models import vcf_results_model as model
 
 TARGET_COLUMNS = [
@@ -20,6 +21,7 @@ TARGET_COLUMNS = [
     "IMPACT",
 ]
 
+META_FILE = "results_meta.json"
 
 # Taken from https://github.com/Ensembl/ensembl-hypsipyle
 # main/common/file_model/variant.py#L142
@@ -51,12 +53,10 @@ def _set_allele_type(alt_one_bp: bool, ref_one_bp: bool, ref_alt_equal_bp: bool)
 
 
 def _get_prediction_index_map(
-    csq_header: str, target_columns: List[str] = None
-) -> Dict:
+    csq_header: str, target_columns: list[str] = TARGET_COLUMNS
+) -> dict[str, int]:
     """Creates a dictionary of column indexes based
     on the CSQ info description"""
-    if not target_columns:
-        target_columns = TARGET_COLUMNS
     csq_header = csq_header.split(":")[-1].strip()
     csq_headers = csq_header.split("|")
 
@@ -66,13 +66,15 @@ def _get_prediction_index_map(
         if csq_headers[index] in target_columns
     }
 
-    # TODO add exception if len(map) != len(TARGET_COLUMNS)
+    if len(map_index) != len(target_columns):
+        raise Exception(f"Required columns missing from CSQ header: {map_index.keys()}")
+
     return map_index
 
 
 def _get_csq_value(
-    csq_values: List, csq_key: str, default_value: Any, index_map: Dict
-) -> Any:
+    csq_values: list[str], csq_key: str, default_value: str | None, index_map: dict[str, int]
+):
     """Helper method to return CSQ values or a default value
     if either the key or the value is missing"""
     if csq_key in index_map and csq_values[index_map[csq_key]]:
@@ -81,7 +83,7 @@ def _get_csq_value(
 
 
 def _get_alt_allele_details(
-    alt: str, ref: str, csqs: List[str], index_map: Dict
+    alt: str, ref: str, csqs: list[str], index_map: dict[str, int]
 ) -> model.AlternativeVariantAllele:
     """Creates  AlternativeVariantAllele based on
     target alt allele and CSQ entires"""
@@ -119,9 +121,9 @@ def _get_alt_allele_details(
             consequences.append(
                 model.PredictedTranscriptConsequence(
                     feature_type=model.FeatureType.transcript,
-                    stable_id=_get_csq_value(csq_values, "Feature", None, index_map),
-                    gene_stable_id=_get_csq_value(csq_values, "Gene", None, index_map),
-                    biotype=_get_csq_value(csq_values, "BIOTYPE", None, index_map),
+                    stable_id=_get_csq_value(csq_values, "Feature", "", index_map),
+                    gene_stable_id=_get_csq_value(csq_values, "Gene", "", index_map),
+                    biotype=_get_csq_value(csq_values, "BIOTYPE", "", index_map),
                     is_canonical=is_cononical,
                     gene_symbol=_get_csq_value(csq_values, "SYMBOL", None, index_map),
                     consequences=cons,
@@ -144,29 +146,70 @@ def _get_alt_allele_details(
     )
 
 
+def _get_meta(vcf_path: FilePath) -> model.VcfMetadata:
+    """Helper method to manage metainfo for a VCF file"""
+
+    meta_path = vcf_path.with_name(META_FILE)
+    if not meta_path.exists():
+        variant_count_str = subprocess.check_output(
+            f"bcftools stats {vcf_path} | grep 'number of records:'",
+            shell=True, text=True
+        )
+        header_count_str = subprocess.check_output(
+            f"bcftools view -h {vcf_path} | wc -l",
+            shell=True, text=True
+        )
+        vcf_info = model.VcfMetadata(
+            variant_count=int(variant_count_str.split(":")[-1]),
+            header_count=int(header_count_str)
+        )
+        with open(meta_path, "w") as meta_file:
+            meta_file.write(vcf_info.model_dump_json())
+    else:
+        with open(meta_path, "r") as meta_file:
+            vcf_info = model.VcfMetadata.model_validate_json(meta_file.read())
+    return vcf_info
+
+
 def get_results_from_path(
-    page_size: int, page: int, vcf_path: str
+    page_size: int, page: int, vcf_path: FilePath
 ) -> model.VepResultsResponse:
-    """Helper method that converts a file path to a stream
-    for use with get_results_from_stream"""
-    # Todo check file file exists
-    vcf_records = vcfpy.Reader.from_path(vcf_path)
-    return _get_results_from_vcfpy(page_size, page, vcf_records)
+    """Returns a page of VCF data from the given filepath.
+    Slices the input VCF file to a smaller one
+    and converts it to stream for get_results_from_stream"""
+    if not vcf_path.exists():
+        raise Exception(f"VEP results file not found at {vcf_path}")
+
+    # Fetch a pageful of variant records with headers
+    vcf_info = _get_meta(vcf_path)
+    total = vcf_info.variant_count
+    page = max(page, 1) # normalize values
+    page_size = min(max(page_size, 0), total)
+    row_offset = min(page * page_size, total) + vcf_info.header_count
+    vcf_headers = subprocess.check_output( # fetch all header rows
+        f"bcftools view -h {vcf_path}", shell=True, text=True
+    )
+    vcf_slice = subprocess.check_output( # fetch subset of variant rows
+        f"bcftools view {vcf_path} | head -n{row_offset} | tail -n{page_size}",
+        shell=True, text=True
+    )
+    vcf_stream = StringIO(vcf_headers + vcf_slice)
+
+    return get_results_from_stream(page_size, page, total, vcf_stream)
 
 
 def get_results_from_stream(
-    page_size: int, page: int, vcf_stream: Any
+    page_size: int, page: int, total: int, vcf_stream: StringIO
 ) -> model.VepResultsResponse:
-    """Generates a page of VCF data in the format described in
-    APISpecification.yaml for a given VCF stream"""
+    """Helper method to convert a filestream to VCF records for _get_results_from_vcfpy"""
 
     # Load vcf
     vcf_records = vcfpy.Reader.from_stream(vcf_stream)
-    return _get_results_from_vcfpy(page_size, page, vcf_records)
+    return _get_results_from_vcfpy(page_size, page, total, vcf_records)
 
 
 def _get_results_from_vcfpy(
-    page_size: int, page: int, vcf_records: vcfpy.Reader
+    page_size: int, page: int, total: int, vcf_records: vcfpy.Reader
 ) -> model.VepResultsResponse:
     """Generates a page of VCF data in the format described in
     APISpecification.yaml for a given VCFPY reader"""
@@ -176,85 +219,51 @@ def _get_results_from_vcfpy(
         vcf_records.header.get_info_field_info("CSQ").description
     )
 
-    # handle offset
-    count = 0
-    if page < 1:
-        page = 1
-    offset = page_size * (page - 1)
-
-    # This is very slow. We need to find a better way of handling this.
-    # vcfpy __next__ might be the key as it reads lines
-    if offset > 0:
-        for _r in vcf_records:
-            count += 1
-            if count >= offset:
-                break
-                
-    #user asked for a page out of range
-    if offset > count:
-        return model.VepResultsResponse(
-                metadata=model.Metadata(
-                    pagination=model.PaginationMetadata(
-                        page=page, per_page=page_size, total=count
-                    )
-                ),
-                variants=[],
-        )        
-
-    # build page
     variants = []
-    page_count = 0
+    # populate variants page
+    if page*page_size <= total:
+        for record in vcf_records:
+            if record is None:
+                break
+            if record.CHROM.startswith("chr"):
+                record.CHROM = record.CHROM[3:]
 
-    # populate page
-    for record in vcf_records:
-        if record.CHROM.startswith("chr"):
-            record.CHROM = record.CHROM[3:]
+            ref_len = len(record.REF)
 
-        ref_len = len(record.REF)
-
-        # https://github.com/bihealth/vcfpy/blob/697768d032b6b476766fb4c524c91c8d24559330/vcfpy/record.py#L63
-        # end does not look like it is implemented.
-        # https://github.com/Penghui-Wang/PyVCF/blob/master/vcf/model.py#L190
-        # from competting vcf module
-        location = model.Location(
-            region_name=record.CHROM,
-            start=record.POS,
-            end=record.POS + ref_len,
-        )
-        longest_alt = len(max((a.value for a in record.ALT), key=len))
-
-        alt_allele_strings = list(set([
-            csq_string.split("|")[prediction_index_map["Allele"]]
-            for csq_string in record.INFO["CSQ"]
-        ]))
-
-        alt_alleles = [
-            _get_alt_allele_details(alt, record.REF, record.INFO["CSQ"], prediction_index_map)
-            for alt in alt_allele_strings
-        ]
-
-        variants.append(
-            model.Variant(
-                name=";".join(record.ID) if len(record.ID) > 0 else ".",
-                location=location,
-                reference_allele=model.ReferenceVariantAllele(
-                    allele_sequence=record.REF
-                ),
-                alternative_alleles=alt_alleles,
-                allele_type=_set_allele_type(
-                    longest_alt < 2, ref_len < 2, longest_alt == ref_len
-                )[0],
+            # https://github.com/bihealth/vcfpy/blob/697768d032b6b476766fb4c524c91c8d24559330/vcfpy/record.py#L63
+            # end does not look like it is implemented.
+            # https://github.com/Penghui-Wang/PyVCF/blob/master/vcf/model.py#L190
+            # from competing vcf module
+            location = model.Location(
+                region_name=record.CHROM,
+                start=record.POS,
+                end=record.POS + ref_len,
             )
-        )
+            longest_alt = len(max((a.value for a in record.ALT), key=len))
 
-        page_count += 1
-        if page_count >= page_size:
-            break
+            alt_allele_strings = list(set([
+                csq_string.split("|")[prediction_index_map["Allele"]]
+                for csq_string in record.INFO["CSQ"]
+            ]))
 
-    # Also very slow. We could compute this and add it to the VCF header
-    total = offset + page_count
-    for _r in vcf_records:
-        total += 1
+            alt_alleles = [
+                _get_alt_allele_details(alt, record.REF, record.INFO["CSQ"], prediction_index_map)
+                for alt in alt_allele_strings
+            ]
+
+            variants.append(
+                model.Variant(
+                    name=";".join(record.ID) if len(record.ID) > 0 else ".",
+                    location=location,
+                    reference_allele=model.ReferenceVariantAllele(
+                        allele_sequence=record.REF
+                    ),
+                    alternative_alleles=alt_alleles,
+                    allele_type=_set_allele_type(
+                        longest_alt < 2, ref_len < 2, longest_alt == ref_len
+                    )[0],
+                )
+            )
 
     return model.VepResultsResponse(
         metadata=model.Metadata(

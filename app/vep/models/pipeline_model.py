b454d596bd74cfde8bd38a3d03a34ecff489a0a3
Andres Veidenberg <andres@ebi.ac.uk>
2026-06-08 15:04:43 +0300

    Edit VEP version

--- app/vep/models/pipeline_model.py
+++ app/vep/models/pipeline_model.py
@@ -27,7 +27,7 @@ class VEPConfigParams(BaseModel):
     outdir: DirectoryPath
     bin_size: int = 3000
     sort: bool = True
-    vep_version: str = "115"
+    vep_version: str = "115.0"
 
     @model_serializer
     def vep_config_serialiser(self):
